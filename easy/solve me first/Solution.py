57024a65e06454150af89ac9f826326fae4d7066
Gagandeep Kaur <gagandeepkaur7099@gmail.com>
2026-07-05 16:49:55 +0530

    Delete easy/solve me first directory

--- easy/solve me first/Solution.py
+++
@@ -1,8 +0,0 @@
-def solveMeFirst(a, b):
-    return a + b
-
-
-a = int(input())
-b = int(input())
-
-print(solveMeFirst(a, b))
--- easy/solve me first/simple array sum/Solution.py
+++
@@ -1,8 +0,0 @@
-def simpleArraySum(ar):
-    return sum(ar)
-
-
-n = int(input())
-ar = list(map(int, input().split()))
-
-print(simpleArraySum(ar))
